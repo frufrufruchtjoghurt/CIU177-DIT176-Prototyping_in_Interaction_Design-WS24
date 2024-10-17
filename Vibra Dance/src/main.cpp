8de96fc4a3a34d1e90651818108071b15425096a
Markus Fruhmann <fruhmann@student.chalmers.se>
2024-10-17 11:15:16 +0200

    Implemented and synchronized vibration to LED strip

--- Vibra Dance/src/main.cpp
+++ Vibra Dance/src/main.cpp
@@ -12,21 +12,28 @@
 #define UPDATES_PER_SECOND 100
 CRGB leds[NUM_LEDS];
 
+#define VIBRATION_PIN 3
+#define VIBRATION_STRENGTH 135
+#define VIBRATION_TIME 200
+
 CRGBPalette16 currentPalette;
 TBlendType currentBlending;
 
-uint16_t vibrationTime = 200;
-uint16_t bpm = 0;
+uint16_t bpm;
+uint16_t bpmInterval;
 
 // put function declarations here:
+uint16_t bpmToTime(uint16_t bpm);
 uint8_t calculateBrightness();
 uint8_t calculateColorIndex();
 void FillLEDsFromPaletteColors(uint8_t colorIndex, uint8_t brightness);
 CRGBPalette16 generateRedAndPurplePalette();
-CRGBPalette16 generateBlueAndDarkGreenPalette();
+CRGBPalette16 generateBlueAndDarkVioletPalette();
 
 void setup()
 {
+  pinMode(VIBRATION_PIN, OUTPUT);
+
   // put your setup code here, to run once:
   FastLED.addLeds<LED_TYPE, LED_STRIP_PIN>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
@@ -34,27 +41,28 @@ void setup()
   FastLED.clear();
   FastLED.show();
 
-  currentPalette = generateBlueAndDarkGreenPalette();
+  currentPalette = generateRedAndPurplePalette();
   currentBlending = LINEARBLEND;
 
-  bpm = SAD_SONG_BPM;
+  bpm = STAYING_ALIVE_BPM;
+  bpmInterval = bpmToTime(bpm);
 }
 
 void loop()
 {
-  /*
-  uint8_t colorIndex = calculateColorIndex();
-  uint8_t brightness = calculateBrightness();
-
-  FillLEDsFromPaletteColors(colorIndex, brightness);
-
-  FastLED.show();
-  FastLED.delay(1000 / UPDATES_PER_SECOND);
-  */
-
   // change brightness and color according to the beat
   uint8_t brightness = beatsin8(bpm, 5, 255);
   uint8_t colorIndex = beatsin8(bpm, 0, 16);
+  uint8_t vibrate = beatsin8(bpm, 0, 100, -(bpmInterval / 90));
+
+  if (vibrate > 70)
+  {
+    analogWrite(VIBRATION_PIN, VIBRATION_STRENGTH);
+  }
+  else
+  {
+    analogWrite(VIBRATION_PIN, 0);
+  }
 
   FillLEDsFromPaletteColors(colorIndex, brightness);
 
@@ -100,7 +108,7 @@ CRGBPalette16 generateRedAndPurplePalette()
   return palette;
 }
 
-CRGBPalette16 generateBlueAndDarkGreenPalette()
+CRGBPalette16 generateBlueAndDarkVioletPalette()
 {
   CRGBPalette16 palette = CRGBPalette16(
       CRGB::Blue,
